692d2825fcc0af01ae7fb9f6810e6120829dfa42
Dyego Maas <dyego.maas@gmail.com>
2020-06-12 16:24:46 -0300

    Adjustment.

--- bin/api.js
+++ bin/api.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var hugolunr = require('../');
 var indexer = new hugolunr();
-indexer.setInput('content/faq/**');
-indexer.setOutput('public/faq.json');
+indexer.setInput('d:\\projects\\blog\\content\\posts\\**');
+indexer.setOutput('faq.json');
+indexer.setIncludeFuture(false)
 indexer.index();
